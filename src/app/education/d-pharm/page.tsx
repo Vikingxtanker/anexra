a27eba6211ac02df908c35026c0e292194027296
Vikingxtanker <vikingxtanker@gmail.com>
2026-05-26 17:13:17 +0530

    education add v2

---
+++ src/app/education/d-pharm/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/sections/Navbar";
+import Footer from "@/sections/Footer";
+
+export default function Home() {
+    return <>
+        <Navbar/>
+
+        <Footer/>
+      </>;
+}
