8103b1391ae333c44f54b1ee3e7cd50d157f0507
Vikingxtanker <vikingxtanker@gmail.com>
2026-05-29 01:01:57 +0530

    dashboard v2

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-
 import StudentNavbar from "@/components/student-navbar";
 
 export default function StudentLayout({
@@ -9,23 +7,11 @@ export default function StudentLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  const isAuthPage =
-    pathname === "/student/login" ||
-    pathname === "/student/register";
-
   return (
     <>
       <StudentNavbar />
 
-      <main
-        className={
-          isAuthPage
-            ? ""
-            : "pt-32"
-        }
-      >
+      <main>
         {children}
       </main>
     </>
