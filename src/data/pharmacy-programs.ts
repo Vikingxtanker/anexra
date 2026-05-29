0607b2698898deee5f7e890aa6939c0c0c815b90
Vikingxtanker <vikingxtanker@gmail.com>
2026-05-29 08:57:22 +0530

    degree cards syllabus update

--- src/data/pharmacy-programs.ts
+++ src/data/pharmacy-programs.ts
@@ -35,8 +35,8 @@ export const pharmacyPrograms = {
             slug: "pharmacology",
           },
           {
-            name: "Community Pharmacy",
-            slug: "community-pharmacy",
+            name: "Community Pharmacy & Management",
+            slug: "community-pharmacy-management",
           },
           {
             name: "Biochemistry & Clinical Pathology",
@@ -63,14 +63,189 @@ export const pharmacyPrograms = {
     duration: "8 Semesters",
 
     semesters: {
-      "1st Semester": [],
-      "2nd Semester": [],
-      "3rd Semester": [],
-      "4th Semester": [],
-      "5th Semester": [],
-      "6th Semester": [],
-      "7th Semester": [],
-      "8th Semester": [],
+      "1st Semester": {
+        subjects: [
+          {
+            name: "Human Anatomy and Physiology I",
+            slug: "human-anatomy-physiology-1",
+          },
+          {
+            name: "Pharmaceutical Analysis I",
+            slug: "pharmaceutical-analysis-1",
+          },
+          {
+            name: "Pharmaceutics I",
+            slug: "pharmaceutics-1",
+          },
+          {
+            name: "Pharmaceutical Inorganic Chemistry",
+            slug: "pharmaceutical-inorganic-chemistry-1",
+          },
+        ],
+      },
+
+      "2nd Semester": {
+        subjects: [
+          {
+            name: "Human Anatomy and Physiology II",
+            slug: "human-anatomy-physiology-2",
+          },
+          {
+            name: "Pharmaceutical Organic Chemistry I",
+            slug: "pharmaceutical-organic-chemistry-1",
+          },
+          {
+            name: "Biochemistry",
+            slug: "biochemistry",
+          },
+          {
+            name: "Pathophysiology",
+            slug: "pathophysiology",
+          },
+          {
+            name: "Computer Applications in Pharmacy",
+            slug: "computer-applications-in-pharmacy",
+          },
+          {
+            name: "Environmental Sciences",
+            slug: "environmental-sciences",
+          },
+        ],
+      },
+
+      "3rd Semester": {
+        subjects: [
+          {
+            name: "Pharmaceutical Organic Chemistry II",
+            slug: "pharmaceutical-organic-chemistry-2",
+          },
+          {
+            name: "Physical Pharmaceutics",
+            slug: "physical-pharmaceutics",
+          },
+          {
+            name: "Pharmaceutical Microbiology",
+            slug: "pharmaceutical-microbiology",
+          },
+          {
+            name: "Pharmaceutical Engineering",
+            slug: "pharmaceutical-engineering",
+          },
+        ],
+      },
+
+      "4th Semester": {
+        subjects: [
+          {
+            name: "Pharmaceutical Organic Chemistry III",
+            slug: "pharmaceutical-organic-chemistry-3",
+          },
+          {
+            name: "Medicinal Chemistry I",
+            slug: "medicinal-chemistry-1",
+          },
+          {
+            name: "Physical Pharmaceutics II",
+            slug: "physical-pharmaceutics-2",
+          },
+          {
+            name: "Pharmacology I",
+            slug: "pharmacology-1",
+          },
+          {
+            name: "Pharmacognosy and Phytochemistry I",
+            slug: "pharmacognosy-phytochemistry-1",
+          },
+        ],
+      },
+
+      "5th Semester": {
+        subjects: [
+          {
+            name: "Medicinal Chemistry II",
+            slug: "medicinal-chemistry-2",
+          },
+          {
+            name: "Industrial Pharmacy I",
+            slug: "industrial-pharmacy-1",
+          },
+          {
+            name: "Pharmacology II",
+            slug: "pharmacology-2",
+          },
+          {
+            name: "Pharmacognosy and Phytochemistry II",
+            slug: "pharmacognosy-phytochemistry-2",
+          },
+          {
+            name: "Pharmaceutical Jurisprudence",
+            slug: "pharmaceutical-jurisprudence",
+          },
+        ],
+      },
+
+      "6th Semester": {
+        subjects: [
+          {
+            name: "Medicinal Chemistry III",
+            slug: "medicinal-chemistry-3",
+          },
+          {
+            name: "Pharmacology III",
+            slug: "pharmacology-3",
+          },
+          {
+            name: "Herbal Drug Technology",
+            slug: "herbal-drug-technology",
+          },
+          {
+            name: "Biopharmaceutics and Pharmacokinetics",
+            slug: "biopharmaceutics-pharmacokinetics",
+          },
+          {
+            name: "Pharmaceutical Biotechnology",
+            slug: "pharmaceutical-biotechnology",
+          },
+          {
+            name: "Quality Assurance",
+            slug: "quality-assurance",
+          },
+        ],
+      },
+
+      "7th Semester": {
+        subjects: [
+          {
+            name: "Instrumental Methods of Analysis",
+            slug: "instrumental-methods-of-analysis",
+          },
+          {
+            name: "Industrial Pharmacy II",
+            slug: "industrial-pharmacy-2",
+          },
+          {
+            name: "Pharmacy Practice",
+            slug: "pharmacy-practice",
+          },
+          {
+            name: "Novel Drug Delivery System",
+            slug: "novel-drug-delivery-system",
+          },
+        ],
+      },
+
+      "8th Semester": {
+        subjects: [
+          {
+            name: "Biostatistics and Research Methodology",
+            slug: "biostatistics-research-methodology",
+          },
+          {
+            name: "Social and Preventive Pharmacy",
+            slug: "social-preventive-pharmacy",
+          },
+        ],
+      },
     },
   },
 
@@ -78,11 +253,134 @@ export const pharmacyPrograms = {
     duration: "6 Years",
 
     years: {
-      "1st Year": [],
-      "2nd Year": [],
-      "3rd Year": [],
-      "4th Year": [],
-      "5th Year": [],
+      "1st Year": {
+        subjects: [
+          {
+            name: "Human Anatomy and Physiology",
+            slug: "human-anatomy-and-physiology",
+          },
+          {
+            name: "Pharmaceutics",
+            slug: "pharmaceutics",
+          },
+          {
+            name: "Medicinal Biochemistry",
+            slug: "medicinal-biochemistry",
+          },
+          {
+            name: "Pharmaceutical Organic Chemistry",
+            slug: "pharmaceutical-organic-chemistry",
+          },
+          {
+            name: "Pharmaceutical Inorganic Chemistry",
+            slug: "pharmaceutical-inorganic-chemistry",
+          },
+        ],
+      },
+
+      "2nd Year": {
+        subjects: [
+          {
+            name: "Pathophysiology",
+            slug: "pathophysiology",
+          },
+          {
+            name: "Pharmaceutical Microbiology",
+            slug: "pharmaceutical-microbiology",
+          },
+          {
+            name: "Pharmacognosy and Phytopharmaceuticals",
+            slug: "pharmacognosy-and-phytopharmaceuticals",
+          },
+          {
+            name: "Pharmacology I",
+            slug: "pharmacology-1",
+          },
+          {
+            name: "Community Pharmacy",
+            slug: "community-pharmacy",
+          },
+          {
+            name: "Pharmacotherapeutics I",
+            slug: "pharmacotherapeutics-1",
+          },
+        ],
+      },
+
+      "3rd Year": {
+        subjects: [
+          {
+            name: "Pharmacology II",
+            slug: "pharmacology-2",
+          },
+          {
+            name: "Pharmaceutical Analysis",
+            slug: "pharmaceutical-analysis",
+          },
+          {
+            name: "Pharmacotherapeutics II",
+            slug: "pharmacotherapeutics-2",
+          },
+          {
+            name: "Pharmaceutical Jurisprudence",
+            slug: "pharmaceutical-jurisprudence",
+          },
+          {
+            name: "Medicinal Chemistry",
+            slug: "medicinal-chemistry",
+          },
+          {
+            name: "Pharmaceutical Formulations",
+            slug: "pharmaceutical-formulations",
+          },
+        ],
+      },
+
+      "4th Year": {
+        subjects: [
+          {
+            name: "Pharmacotherapeutics III",
+            slug: "pharmacotherapeutics-3",
+          },
+          {
+            name: "Hospital Pharmacy",
+            slug: "hospital-pharmacy",
+          },
+          {
+            name: "Clinical Pharmacy",
+            slug: "clinical-pharmacy",
+          },
+          {
+            name: "Biostatistics and Research Methodology",
+            slug: "biostatistics-and-research-methodology",
+          },
+          {
+            name: "Biopharmaceutics and Pharmacokinetics",
+            slug: "biopharmaceutics-and-pharmacokinetics",
+          },
+          {
+            name: "Clinical Toxicology",
+            slug: "clinical-toxicology",
+          },
+        ],
+      },
+
+      "5th Year": {
+        subjects: [
+          {
+            name: "Clinical Research",
+            slug: "clinical-research",
+          },
+          {
+            name: "Pharmacoepidemiology and Pharmacoeconomics",
+            slug: "pharmacoepidemiology-and-pharmacoeconomics",
+          },
+          {
+            name: "Clinical Pharmacokinetics and Pharmacotherapeutic Drug Monitoring",
+            slug: "clinical-pharmacokinetics-and-pharmacotherapeutic-drug-monitoring",
+          },
+        ],
+      },
     },
   },
 };
